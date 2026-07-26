8a93860d1c4e356118b41803ac68227bd30c2a2e
박찬빈 <chanbin0626@gmail.com>
2026-07-26 20:13:53 +0900

    fix: 스토리북 빌드의 public 이중 복사 경쟁 제거 (#56) (#57)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,12 @@ const config: StorybookConfig = {
   addons: ["@storybook/addon-docs", "@storybook/addon-a11y"],
   framework: "@storybook/nextjs-vite",
   staticDirs: ["../public"],
+  // Vite의 publicDir 복사를 꺼요. staticDirs와 같은 public을 동시에 복사해서,
+  // public에 하위 폴더가 있으면 mkdir가 경쟁하다 EEXIST로 빌드가 깨져요. 복사는 staticDirs 한 곳만 해요.
+  viteFinal: (viteConfig) => {
+    viteConfig.publicDir = false;
+    return viteConfig;
+  },
 };
 
 export default config;
